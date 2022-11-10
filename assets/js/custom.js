21f1b57283a545933fcc79de3e9fdb4e536d9b4c
吳家榮 <udngroup.rain.wu@gmail.com>
2022-11-10 11:26:24 +0800

    add imgur api

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,7 @@
 (function ($) {
 	
 	"use strict";
+
 	$('.owl-men-item').owlCarousel({
 		items:5,
 		loop:true,
@@ -78,6 +79,41 @@
 	// Scroll animation init
 	window.sr = new scrollReveal();
 	
+	let leftFirstImages = [
+		"https://i.imgur.com/e2g6VFi.jpg"
+	]
+	let rightFirstInfos = [
+		{
+			name: '大胖',
+			localtion: '碇內吊橋',
+			context: 'hello',
+			url: "#",
+			image: "https://i.imgur.com/Ia0XceQ.jpg",
+		},
+		{
+			name: '大胖',
+			localtion: '碇內吊橋',
+			context: 'hello',
+			url: "#",
+			image: "https://i.imgur.com/PtXWv3C.jpg",
+		},
+		{
+			name: '大胖',
+			localtion: '碇內吊橋',
+			context: 'hello',
+			url: "#",
+			image: "https://i.imgur.com/i0wYyJT.jpg",
+		},
+		{
+			name: '大胖',
+			localtion: '碇內吊橋',
+			context: 'hello',
+			url: "#",
+			image: "https://i.imgur.com/y0sfdvF.jpg"
+		}
+
+	]
+	let menInfos = []
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
@@ -107,9 +143,22 @@
 		}
 	});
 
-	$(document).ready(function () {
-	    $(document).on("scroll", onScroll);
-	    
+	$(document).ready(  function  () {
+	    // $(document).on("scroll", onScroll);
+			const album = '24xI7sL'
+			const settingsList = {
+				"async": true,
+				"crossDomain": true,
+				"url": 	`https://api.imgur.com/3/album/${album}/images`,
+				"method": "GET",
+				"headers": {
+					"Authorization": 'Bearer ' + ACCESSTOKEN
+				}
+			}
+			$.ajax(settingsList).done(
+				function(res)  { 
+					menInfos = res.data
+					console.log(menInfos, 'data')
 	    //smoothscroll
 	    $('.scroll-to-section a[href^="#"]').on('click', function (e) {
 	        e.preventDefault();
@@ -130,7 +179,69 @@
 	            $(document).on("scroll", onScroll);
 	        });
 	    });
+
+			      // Right up images 
+				const row = $('#right-content').children('.row')
+				$.each(rightFirstInfos, (i, v) => {
+					const contenet = `<div class="col-lg-6">
+																			<div class="right-first-image">
+																			<div class="thumb">
+																					<div class="inner-content">
+																							<h4>${v.name}</h4>
+																							<span>${v.localtion}</span>
+																					</div>
+																					<div class="hover-content">
+																							<div class="inner">
+																									<h4>${v.name}</h4>
+																									<p>${v.name}
+																									</p>
+																									98989
+																									<div class="main-border-button">
+																											<a href="${v.url}">Discover More</a>
+																									</div>
+																							</div>
+																					</div>
+																					<img src ='${v.image}'>
+																			</div>
+																		</div>
+																	</div>`
+					row.prepend(contenet)
+				})
+	
+						let arr = []
+						$.each(menInfos, (i, v) => {
+							console.log(v, '1')
+							let contenet = `<div class="item">
+																<div class="thumb">
+																	<div class="hover-content">
+																		<ul>
+																			<li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+																		</ul>
+																	</div>
+																	<img src='${v.link}'>
+																</div>
+																<div class="down-content">
+																	<h4>Classic Spring</h4>
+																	<span>$110.00</span>
+																	<ul class="stars">
+																		<li><i class="fa fa-star"></i></li>
+																		<li><i class="fa fa-star"></i></li>
+																		<li><i class="fa fa-star"></i></li>
+																		<li><i class="fa fa-star"></i></li>
+																		<li><i class="fa fa-star"></i></li>
+																	</ul>
+																</div>
+															</div>`
+								$('.owl-men-item').trigger('add.owl.carousel', contenet)
+								$('.owl-women-item').trigger('add.owl.carousel', contenet)
+								$('.owl-kid-item').trigger('add.owl.carousel', contenet)
+						})
+			
+						$('.owl-men-item').trigger('refresh.owl.carousel');
+						$('.owl-women-item').trigger('refresh.owl.carousel');
+						$('.owl-kid-item').trigger('refresh.owl.carousel');
 	});
+	})
 
 	function onScroll(event){
 	    var scrollPos = $(document).scrollTop();
@@ -156,14 +267,14 @@
 				zIndex: '1'
 			});
 		}
-
-		$("#preloader").animate({
-			'opacity': '0'
-		}, 600, function(){
-			setTimeout(function(){
-				$("#preloader").css("visibility", "hidden").fadeOut();
-			}, 300);
-		});
+		$("#preloader").css("visibility", "hidden")
+		// $("#preloader").animate({
+		// 	'opacity': '0'
+		// }, 600, function(){
+		// 	setTimeout(function(){
+		// 		$("#preloader").css("visibility", "hidden").fadeOut();
+		// 	}, 300);
+		// });
 	});
 
 
@@ -183,6 +294,40 @@
 			}
 		});
 	}
+	const ACCESSTOKEN = 'bd0cb9b01c3f3cae3322ccce3e4a47d9bea4029b'
+	const REFRESHTOKEN = '7237de6b05ba4da652b1c740a690e3d96724986f'
+	const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjVkMzQwZGRiYzNjNWJhY2M0Y2VlMWZiOWQxNmU5ODM3ZWM2MTYzZWIiLCJ0eXAiOiJKV1QifQ.eyJwcm92aWRlcl9pZCI6ImFub255bW91cyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9hZ2VudC1qaW5mbS10ZXN0LWFiZ3FydCIsImF1ZCI6ImFnZW50LWppbmZtLXRlc3QtYWJncXJ0IiwiYXV0aF90aW1lIjoxNjY1NjU0ODkzLCJ1c2VyX2lkIjoidEdCTlA1aGk1SFNlU3Voc3VxYWFnRFNPb1J2MiIsInN1YiI6InRHQk5QNWhpNUhTZVN1aHN1cWFhZ0RTT29SdjIiLCJpYXQiOjE2NjYzMzYwNTQsImV4cCI6MTY2NjMzOTY1NCwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6e30sInNpZ25faW5fcHJvdmlkZXIiOiJhbm9ueW1vdXMifX0.Y-s4nyqFHXMzpFgAvKCUwmhwibN-YesHYz2gKM49Iqsx9uFEl91lu-43vioKhsidBQScsn754AS3WLDsefXe19Te1o06A8ReqtXwEXtw_RGHJ2eTlSMcWuJ86G5aJDdZJSWCvqvMp61qqDcP1teQZ6OQYy-sXaTo59wOFndfrYe91JjJOF3xPNaJ-3P4FIXfojLCHwYf1EG7AZrkiS3PKs6HmFBLBz1nZbi3jmgZkERbdvGBhZoDambY1dXEaKMpVxq8FGHChSq4TsD5UsTDCld9Z7gSgdfZz9EaO7YRqigBZtnpr87rMU77ICNLtl56KkZk4IHxalhDjF5ZrAulhg'
+	function getImage() {
+
+	}
+
+	// function submit() {
+	// 	let settings = {
+	// 		async: true,
+	// 		crossDomain: true,
+	// 		processData: false,
+	// 		contentType: false,
+	// 		type: 'POST',
+	// 		url: 'https://api.imgur.com/3/image',
+	// 		headers: {
+	// 			Authorization: 'Bearer ' +  ACCESSTOKEN
+	// 		},
+	// 		mimeType: 'multipart/form-data'
+	// 	};
+
+	// 	let form = new FormData();
+	// 	form.append('image', this.file);
+	// 	form.append('title', this.title);
+	// 	form.append('description', this.des);
+	// 	form.append('album', album); // 有要指定的相簿就加這行
+
+	// 	settings.data = form;
+
+	// 	$.ajax(settings).done(function(res) {
+	// 		console.log(res); // 可以看見上傳成功後回的值
+	// 		alert('上傳完成，稍待一會兒就可以在底部的列表上看見了。')
+	// 	});
+	// }
 
 
 })(window.jQuery);
